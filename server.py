7657abc35d01763dc699c9a8cfb7ad403aed9a0a
naadavis <nicholasaadavis@gmail.com>
2015-06-20 18:14:36 -0700

    Cleaned and commented python server code.

--- server.py
+++ server.py
@@ -31,16 +31,17 @@ def getOrderedRecs(user,sets):
 prof_dict = {}
 
 # file to get profile data from
-#f = open('musicData3.tsv','r')
-#f = open('smallData.tsv','r')
 f = open('bandData.tsv','r')
 
+# Gets user number from file uses it as a dictionary key
+# the dictionary value is the set of bands/artists
 for line in f:
 	l = line.rstrip('\n').split('\t')
 	prof_dict[int(l[0])] = set(l[1:])
 
 f.close()
 
+# Gets Both Mens and Womens names from file, for demo purposes
 men_names = []
 
 f = open('men.txt','r')
@@ -56,43 +57,39 @@ for line in f:
 	women_names.append(l)
 f.close()
 
-print len(women_names)
-print len(men_names)
-
 print "Loaded in ",
 print len(prof_dict),
 print " profiles"
 
 # builds the return JSON object to send back to server
 def getResult(l):
+	# This result dictionary will be received as a JSON object
 	result = {}
 	result["Active"] = l
 	user = set(l)
+	# Calculate Recommendations using top 7 profiles
 	top = topN.TopN(7)
 	# iterate through all profiles, keeping most similar
+	# with the top data structure this is just adding
 	for key, value in prof_dict.iteritems():
 		top.add((key,value),sjaccard( user, value ) )
+	# Assign Fake, Temporary names to the top recommendations
 	mens = list(men_names)
 	womens = list(women_names)
 	names = {}
 	for i in range(0,len(top.data)):
-		print i
 		if ( top.data[i][0][0] % 2 ) == 1:
 			pos = randint(0,len(mens)-1)
-			print pos
 			names[top.data[i][0][0]] = mens[pos]
 			del mens[pos]
 		else:
 			pos = randint(0,len(womens)-1)
-			print pos
 			names[top.data[i][0][0]] = womens[pos]
 			del womens[pos]
+	# Get the Profiles of the top and put them into the "Relevant" within the result structure
 	result["Relevant"] = map( lambda x: [ names[x[0][0]] , list(x[0][1]) ] , top.data )
+	# Get a ordered list of recommendations based on top profiles
 	result["Rec"] = getOrderedRecs(user,map( lambda x: x[0][1], top.data ) )
-	for i in range(0,len(top.data)):
-		print names[top.data[i][0][0]],
-		print top.data[i][1]
-	#top.printSmall()
 	return result
 
 def decode(a):
@@ -100,20 +97,14 @@ def decode(a):
 
 @app.route('/_get_recs')
 def get_recs():
+	# Resolve JSON structure into a list
 	a = request.args.get('length')
 	l = []
 	for i in range(0,int(a)):
 		l.append(request.args.get(str(i)).encode('utf-8'))
-	# old results for testing
-	#r = { "result": l }
-	#r = { "result": [ ['a',l[0]],['b',l[1]],['r',l[0]+l[1]] ] }
-	#print l
 	r = getResult(l)
-	#print r["Relevant"]
-	#print r["Rec"]
 	return jsonify(**r)
 
-# HTML/JS isn't quite prepared yet
 @app.route('/')
 def index():
 	return render_template('index.html')
